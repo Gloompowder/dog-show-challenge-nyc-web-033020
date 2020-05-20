aab8aaea121898e1b9a5aec9015cbe1949b15682
William <willlin1996@gmail.com>
2020-05-20 12:01:39 -0400

    final save

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 // [x]- On page load, render a list of already registered dogs in the table. You can fetch these dogs from http://localhost:3000/dogs.
 // [x]- The dog should be put on the table as a table row. The HTML might look something like this `<tr><td>Dog *Name*</td> <td>*Dog Breed*</td> <td>*Dog Sex*</td> <td><button>Edit</button></td></tr>`
-// []- Make a dog editable. Clicking on the edit button next to a dog should populate the top form with that dog's current information.
-// - On submit of the form, a PATCH request should be made to http://localhost:3000/dogs/:id to update the dog information (including name, breed and sex attributes).
+// [x]- Make a dog editable. Clicking on the edit button next to a dog should populate the top form with that dog's current information.
+// -[x] On submit of the form, a PATCH request should be made to http://localhost:3000/dogs/:id to update the dog information (including name, breed and sex attributes).
 // - Once the form is submitted, the table should reflect the updated dog information. There are many ways to do this. You could search for the table fields you need to edit and update each of them in turn, but we suggest making a new get request for all dogs and rerendering all of them in the table. Make sure this GET happens after the PATCH so you can get the most up-to-date dog information.
 
 document.addEventListener('DOMContentLoaded', () => {
@@ -14,6 +14,7 @@ document.addEventListener('DOMContentLoaded', () => {
     .then((data) => {renderDogs(data)})
 
     function renderDogs(dogData) {
+        dogBody.innerHTML = ""
         dogData.forEach(dog => {
             let dogRow = document.createElement('tr')
             let dogName = document.createElement('td')
@@ -34,10 +35,38 @@ document.addEventListener('DOMContentLoaded', () => {
         })
         document.addEventListener('click', function(event){
             dogData.forEach((dog)=>{
-                if (event.target.id=== `edit-${dog.id}`){
+                if (event.target.id === `edit-${dog.id}`){
                     dogForm.name.value = dog.name 
                     dogForm.sex.value = dog.sex 
                     dogForm.breed.value = dog.breed
+
+
+                    dogForm.addEventListener('submit', function(event){
+
+                        // debugger;
+                        event.preventDefault()
+                        
+                        updateDogData = {
+                            id: dog.id,
+                            name: dogForm.name.value,
+                            sex: dog.sex, 
+                            breed: dog.breed
+                        }
+
+                        fetch(dogUrl+`/${dog.id}`,{
+                            method: "PATCH",
+                            headers: {
+                                'Content-Type': 'application/json',
+                                'Accept': 'application/json'
+                            },
+                            body: JSON.stringify(updateDogData)
+                        })
+
+                        fetch(dogUrl)
+                        .then(response => response.json())
+                        .then(data => {renderDogs(data)})
+
+                    })
                 }
             })
 
